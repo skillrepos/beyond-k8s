551bb4cea073a279e84cb5eb478e817f4e27afb5
brentlaster <bcl@nclasters.org>
2022-06-28 12:41:49 -0400

    update shell scripts

--- tekton/setup-full.sh
+++ tekton/setup-full.sh
@@ -4,11 +4,11 @@
 # puts command line interface in /usr/local/bin
 
 kubectl create ns tekpipe
-kubectl create ns roar
-kubectl create secret -n tekpipe docker-registry regcreds --docker-server=http://localhost:5000/v2 --docker-username=*** --docker-password=***
+kubectl create ns roar-tekton
+#kubectl create secret -n tekpipe docker-registry regcreds --docker-server=http://localhost:5000/v2 --docker-username=*** --docker-password=***
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-curl -LO https://github.com/tektoncd/cli/releases/download/v0.17.1/tkn_0.17.1_Linux_x86_64.tar.gz
-tar xvzf tkn_0.17.1_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.24.0/tkn_0.24.0_Linux_x86_64.tar.gz
+tar xvzf tkn_0.24.0_Linux_x86_64.tar.gz -C . tkn
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
