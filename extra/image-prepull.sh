20502df97343d5861c8955da12c9134f66f11397
brentlaster <Brent.Laster@sas.com>
2022-06-30 22:57:32 -0400

    update image lists

--- extra/image-prepull.sh
+++ extra/image-prepull.sh
@@ -3,4 +3,4 @@ docker pull quay.io/bclaster/roar-web:1.0.1
 docker pull quay.io/bclaster/roar-web:v2
 docker pull quay.io/techupskills/roar-db:v2
 docker pull quay.io/techupskills/roar-web:v1
-docker pull quay.io/techupskills2/roarapp-operator:1.0.0
+docker pull quay.io/techupskills2/roarapp-operator:2.0.0
