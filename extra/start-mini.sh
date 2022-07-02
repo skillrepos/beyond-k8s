f8ad27f5516f7c08201fe5785c48138b9037ef25
brentlaster <bcl@nclasters.org>
2022-07-02 08:46:02 -0400

    udpate start script

--- extra/start-mini.sh
+++ extra/start-mini.sh
@@ -1,7 +1,2 @@
 sudo minikube start --vm-driver=none
-cd ~/istio-1.14.1
-export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo -y
-kubectl create ns argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.2.5/manifests/install.yaml
 
